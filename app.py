54ecc760b671f757e10d31380e684c6689b20de7
Hashir Kashif <hashirisbest1@gmail.com>
2024-01-05 18:22:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask
+from threading import Thread
+import requests
+import time
+def testTask():
+  n=0
+  while n<5:
+    secret = "NDkxOTgyMTQzNjMxOTE3MDU2.GUy3e4.WbXx_cBp9eRr0s9_SJ91FFATDkz6L8XmN2U8L4"
+    data = {"content": "c.gen netflix"}
+    headers = {"authorization": secret}
+    r = requests.post(
+      "https://discord.com/api/v9/channels/1101192501689339974/messages",
+      data=data,
+      headers=headers)
+    time.sleep(4)
+    n=n+1
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    thr = Thread(target=testTask)
     return 'Hello from Koyeb'
 
 
